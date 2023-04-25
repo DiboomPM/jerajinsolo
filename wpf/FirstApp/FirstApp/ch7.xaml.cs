5f3dfa8400b9b407d8e2aeab27748d02b0fefba6
DiboomPM <53811886+DiboomPM@users.noreply.github.com>
2023-04-25 17:20:23 +0900

    WPF 강좌 #12 리스트박스 Listbox 바인

---
+++ wpf/FirstApp/FirstApp/ch7.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstApp
+{
+    /// <summary>
+    /// ch7.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ch7 : Window
+    {
+        public ch7()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyper_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Hyper.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void Hyper_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Hyper.TextDecorations = null;
+        }
+
+        private void Hyper_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+    }
+}
